1b6711d3b57f6b773d58359cb7611629ba82d0ee
Prantzz <69815135+Prantzz@users.noreply.github.com>
2021-03-09 13:24:08 -0300

    Some fade system tweaks, New Scene (MM_RainForest), and pause events!

--- Assets/_Octoblind/Scripts/FadeController.cs
+++ Assets/_Octoblind/Scripts/FadeController.cs
@@ -8,11 +8,15 @@ public class FadeController : MonoBehaviour
 {
     private Image thisImg;
     private GameGlobeData GameCon;
-    float fadeSpeed = 1f;
+    float fadeSpeed;
 
     void Start()
     {
         thisImg = GetComponent<Image>();
+        
+        if (gameObject.tag == "Out") fadeSpeed = 0f;
+        else fadeSpeed = 1f;
+
         GameCon = GameObject.Find("GameGlobeData").GetComponent<GameGlobeData>();
         GameCon.OnSceneStart += GameCon_OnSceneStart;
         GameCon.OnSceneEnd += GameCon_OnSceneEnd;
@@ -20,25 +24,37 @@ public class FadeController : MonoBehaviour
 
     private void GameCon_OnSceneEnd(object sender, System.EventArgs e)
     {
+        GameCon.OnSceneStart -= GameCon_OnSceneStart;
         if (gameObject.tag == "Out")
         {
-            float fadeSpeed = 0;
-            fadeSpeed += 1 * Time.deltaTime;
-            thisImg.color = new Color(thisImg.color.r, thisImg.color.g, thisImg.color.b, fadeSpeed);
             gameObject.SetActive(true);
+            fadeSpeed += 0.8f * Time.deltaTime;
+            thisImg.color = new Color(thisImg.color.r, thisImg.color.g, thisImg.color.b, fadeSpeed);
+            if (fadeSpeed >= 1)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                GameGlobeData.SceneHasEnded = false;
+                GameCon.OnSceneEnd -= GameCon_OnSceneEnd;
+                return;
+            }
         }
         else return;
     }
 
     private void GameCon_OnSceneStart(object sender, System.EventArgs e)
-    {     
+    {
         if (gameObject.tag == "In")
-        {     
-            fadeSpeed -= 0.7f * Time.deltaTime;
-            thisImg.color = new Color(thisImg.color.r, thisImg.color.g, thisImg.color.b, fadeSpeed);
-            if (fadeSpeed <= 0) gameObject.SetActive(false);
-            Debug.Log(fadeSpeed);
-        }
-        else return;
+            {
+                fadeSpeed -= 0.8f * Time.deltaTime;
+                thisImg.color = new Color(thisImg.color.r, thisImg.color.g, thisImg.color.b, fadeSpeed);
+                if (fadeSpeed <= 0)
+                {
+                    gameObject.SetActive(false);
+                    fadeSpeed = 0;
+                    return;
+                }
+            }
+            else if (gameObject.tag == "Out") gameObject.SetActive(false);
+            else return;
     }
 }
--- Assets/_Octoblind/Scripts/GameGlobeData.cs
+++ Assets/_Octoblind/Scripts/GameGlobeData.cs
@@ -9,6 +9,11 @@ public class GameGlobeData : MonoBehaviour
 {
     public event EventHandler OnSceneStart;
     public event EventHandler OnSceneEnd;
+    public event EventHandler OnGamePaused;
+    public event EventHandler OnGameResumed;
+    public event EventHandler OnEscPressed;
+    public static bool SceneHasEnded = false;
+    public static bool IsGamePaused = false;
 
     private void Start()
     {
@@ -17,5 +22,9 @@ public class GameGlobeData : MonoBehaviour
     void Update()
     {
         if (SceneManager.GetActiveScene().isLoaded) OnSceneStart?.Invoke(this, EventArgs.Empty);
+        if (SceneHasEnded) OnSceneEnd?.Invoke(this, EventArgs.Empty);
+        if (IsGamePaused) OnGamePaused?.Invoke(this, EventArgs.Empty);
+        if (!IsGamePaused) OnGameResumed?.Invoke(this, EventArgs.Empty);
+        if (Input.GetKeyDown(KeyCode.Escape)) OnEscPressed?.Invoke(this, EventArgs.Empty);
     }
 }
